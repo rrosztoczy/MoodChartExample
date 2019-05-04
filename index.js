0f87b7873b29d2de733e5b45e636919d2e45170f
rrosztoczy <rrosztoczy@gmail.com>
2019-05-04 19:45:12 -0400

    adding dynamic gradient

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // TODO: Map gradient colors at each point to mood/stress values and levels
+    // TODO: Goal is to map gradient colors at each point to mood/stress values and levels
+    // Only ten levels... so mapping not too tough.... 
+    // So stressGS createLinearGradient(x0, y0, x1, y1) = createLinearGradient(formattedDateTime[0], stressLevel[0], formattedDateTime[1], stressLevel[1])
 
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
 
-    const stressGradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
-    stressGradientStroke.addColorStop(0, "#94d973");
-    stressGradientStroke.addColorStop(0.5, "#fad874");
-    stressGradientStroke.addColorStop(1, "#f49080");
+
+
 
     const moodGradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
     moodGradientStroke.addColorStop(0, "#94d973");
@@ -30,6 +30,14 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("set variables", formattedDateTime)
         console.log("set variables", mood)
         console.log("set variables", stressLevel)
+
+        const stressGradientStroke = await ctx.createLinearGradient(formattedDateTime[0], stressLevel[0], formattedDateTime[1], stressLevel[1]);
+        // For each element in stressLevel, create a stressGradientStroke.addColorStop(point, color)
+        // Point should be... what?
+        // Color should be dynamic based on teh stress value
+        stressGradientStroke.addColorStop(0, "#94d973");
+        stressGradientStroke.addColorStop(0.5, "#fad874");
+        stressGradientStroke.addColorStop(1, "#f49080");
     
         const moodData = {
             labels: formattedDateTime,
